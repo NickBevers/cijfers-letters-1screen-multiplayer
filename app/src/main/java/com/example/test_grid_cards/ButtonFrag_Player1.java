ec23a5118b66abd902a09b41bd42750dcf12ad0b
nickbevers <nick.bevers9@gmail.com>
2021-03-27 20:00:12 +0100

    feat: add both button fragments for 2 players to the application; Add layout files for both buttonfrags

---
+++ app/src/main/java/com/example/test_grid_cards/ButtonFrag_Player1.java
@@ -0,0 +1,14 @@
+package com.example.test_grid_cards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonFrag_Player1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_frag_player1);
+    }
+}
