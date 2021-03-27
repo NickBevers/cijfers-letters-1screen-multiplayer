9d462fdab0be9a7ae4bd10c6599e9c3e912e7c4c
nickbevers <nick.bevers9@gmail.com>
2021-03-27 20:38:48 +0100

    fix: Changed link to layout files

--- app/src/main/java/com/example/test_grid_cards/ButtonFrag_Player2.java
+++ app/src/main/java/com/example/test_grid_cards/ButtonFrag_Player2.java
@@ -1,14 +1,27 @@
 package com.example.test_grid_cards;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ButtonFrag_Player2 extends AppCompatActivity {
+public class ButtonFrag_Player2 extends Fragment {
 
+    public ButtonFrag_Player2() {
+        // Required empty public constructor
+        super(R.layout.button_frag_player2);
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.button_frag_player2);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.button_frag_player1, container, false);
+
+        return v;
+
     }
 }
